f4377a333323e25d3685dabc48663af9753ed3b9
Dwscdv3 <dwscdv3@qq.com>
2015-05-11 16:49:56 +0800

    UI improved

--- af/source/topic.php
+++ af/source/topic.php
@@ -16,7 +16,7 @@ if ($_GET != null) {
     $nick1 = $row1["Nick"] != null ? $row1["Nick"] : "无名氏";
     echo '<div>
     <div id="Head">
-        <h1 class="left">'.$row1["Title"].'<span class="smaller">&nbsp;#<span id="TID">'.$id.'</span></span></h1>
+        <h1 class="left">'.$row1["Title"].'<span class="smaller">&nbsp;&nbsp;&nbsp;#<span id="TID">'.$id.'</span></span></h1>
         <br />
         <div class="right monospace">&nbsp;&nbsp;&nbsp;'.$row1["Time"].'</div>
         <br />
@@ -24,18 +24,17 @@ if ($_GET != null) {
     </div>
     <br /><br /><br /><br />
     <pre class="text-left larger" style="width: 100%;">'.$row1["Content"].'</pre>
-    <br />';
+    <br /><br />';
     while ($row2 = $result2->fetch_array()) {
         $nick2 = $row2["Nick"] != null ? $row2["Nick"] : "无名氏";
         echo '<div class="comment">
     <div>
-        <span class="bold">'.$row2["Title"].'</span>&nbsp;&nbsp;
-        <br />
+        <span class="left bold larger">'.$row2["Title"].'</span>&nbsp;&nbsp;
         <div class="right monospace">&nbsp;&nbsp;&nbsp;'.$row2["Time"].'</div>
         <br />
         <div class="right">'.$row2["UID"].'：'.$nick2.'</div>
     </div>
-    <pre>'.$row2["Content"].'</pre>
+    <pre class="text-left larger" style="width: 100%;">'.$row2["Content"].'</pre>
 
 </div>';
     }
